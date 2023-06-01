9dcdbcb38c7bfff6bdb3bbc7d12660c42262e720
seasons986 <annalin12345@gmail.com>
2023-06-01 16:34:27 +0800

    got the scrollies

--- index.js
+++ index.js
@@ -5,11 +5,27 @@ window.addEventListener("load", () => {
   document.addEventListener("scroll", () => {
     switch (lastScroll > window.scrollY) {
       case (true):
-        console.log('scroll down');
+        console.log('scroll up');
+        break;
       case (false):
         console.log('scroll down');
+        drawStickPerson();
+        break;
+      default:
+        break;
     }
-    console.log('scrolled', window.scrollY);
+    let x = Math.abs(window.scrollY - lastScroll);
+    if (x < 3) console.log('slow');
+    else if (x < 40) console.log('fast');
+    else if (x >= 40) console.log('faster');
     lastScroll = window.scrollY;
   });
+
+  function drawStickPerson() {
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {
+
+      }
+    }
+  }
 });
